b160291248ae65c465be9c43f03875f2a41ff16c
max <bruno.m.voss@gmail.com>
2024-06-07 16:21:29 +0200

    it's working

--- main.py
+++ main.py
@@ -30,6 +30,115 @@ class WorldObject:
         # one entry. best don't change this if you don't want to 
         # dig into how object creation works.
 
+def get_position_elevation(point,cell):
+    # or something.
+    # I do have an average value for the center
+    # or I have a center a specific elevation and then the corner
+    # points are interpolated from the relative distance to 
+    # some of the determining points.
+    # there is probably no point in doing a different interpolation than
+    # linear. If that's too rough, it's probably a better idea
+    # to increase the resolution of the grid, rather than smoothing
+    # position values here.
+    
+    
+    # the outcome should be that the points should get their
+    
+    triangle = find_relevant_triangle(point,cell)
+    
+    dist_sum = 0
+    for x in triangle:
+        dist_sum += x[0]
+    
+    # wait, this is wrong, I want the inverse. the further that
+    # it is away, the less influece it should have.
+    
+    final_elevation = 0
+    for x in triangle:
+        final_elevation += (1-x[0]/dist_sum)*x[1][2]
+    return final_elevation
+    
+
+def bary_centric_interpolation(P,tp1,tp2,tp3):
+    """
+    takes, "P" the point,
+    tp1,tp2,tp3 are the triangle points
+    https://codeplea.com/triangular-interpolation
+    https://stackoverflow.com/questions/8697521/interpolation-of-a-triangle
+    """
+    
+    # theoretically this could be done in parallel
+    y_diff_1_3 = (tp1[1]-tp3[1])
+    y_diff_2_3 = (tp2[1]-tp3[1])
+    y_diff_3_1 = (tp3[1]-tp1[1])
+    y_diff_p_3 = (P[1]-tp3[1])
+    
+    x_diff_1_3 = (tp1[0]-tp3[0])
+    x_diff_3_2 = (tp3[0]-tp2[0])
+    x_diff_p_3 = (P[0]-tp3[0])
+    
+    # ...then this
+    first_product = y_diff_2_3 * x_diff_p_3
+    second_product = x_diff_3_2 * y_diff_2_3
+    third_product = y_diff_2_3 * x_diff_1_3
+    forth_product = x_diff_3_2 * y_diff_1_3
+    fifth_product = (y_diff_3_1 * x_diff_p_3)
+    sixth_product = (x_diff_1_3 * y_diff_p_3)
+    
+    #...then this
+    lower = (third_product + forth_product)
+    
+    # then this
+    W_1 = (first_product + second_product) / lower
+    W_2 = (fifth_product + sixth_product) / lower
+    # then this
+    W_3 = 1-W_1-W_2
+    
+    # I'm using it here for the z value, but the same weights can
+    # be applied to any value you can make to the points
+    # e.g. the delauney triangulation of my voronoi map
+    value = W_1*tp1[2] + W_2*tp2[2] + W_3*tp3[2]
+    
+    return value
+    
+
+def find_relevant_triangle(point,cell):
+    """this is assuming 2d cell
+    
+    it's finding which "fan blade" of my triangle fan, with center
+    vertex is the appropriate one, to interpolate the z elevation
+    from the values of the points or the center.
+    """
+    
+    
+    point=vector.Vector(point[0],point[1],0)
+    dists=[]
+    center2d=vector.Vector(0,0,0)
+    center=vector.Vector(0,0,0)
+    for x in cell:
+        cell_vert = vector.Vector(*x)
+        point2 = vector.Vector(x[0],x[1],0)
+        d = point2 - point
+        d = d.magnitude()
+        dists.append( (d,cell_vert) )
+        center2d += point2
+        center += cell_vert
+    center2d = center2d/len(cell)
+    center = center/len(cell)
+    
+    dists.sort()
+    #print(dists)
+    #input()
+    
+    d = center-point
+    d = d.magnitude()
+    center_tup = (d,center)
+    return dists[0],dists[1],center_tup
+        
+        
+    a = 1
+    
+
 class MyEditor:
     def __init__(self,b):
         # this is necessary because the creator needs to get access
@@ -72,7 +181,7 @@ class MyEditor:
         
         self.light = self.b.render.attachNewNode(Spotlight("Spot"))
         self.light.node().setScene(self.b.render)
-        self.light.node().setShadowCaster(True)
+        #self.light.node().setShadowCaster(True)
         self.light.node().showFrustum()
         self.light.node().getLens().setFov(40)
         self.light.node().getLens().setNearFar(10, 100)
@@ -94,25 +203,171 @@ class MyEditor:
         self.b.render.setShaderAuto()
         
         self.load_saved_river()
+    
+    def recursive_river_tree_unpack(self,my_dict):
+        
+        my_tuple_list = []
+        for x in my_dict:
+            if my_dict[x]!=None:
+                lower_list = self.recursive_river_tree_unpack(my_dict[x])
+                other_keys = list(my_dict[x].keys())
+                for x2 in other_keys:
+                    my_tuple_list.append((x,x2))
+                    my_tuple_list.append((x2,x))
+                my_tuple_list += lower_list
+                
+        return my_tuple_list
         
     def load_saved_river(self):
-        d=sxml_main.read("saved_rivers.xml")
-        d=d["data"]
-        trees=d.pop("rivertrees")
+        d = sxml_main.read("saved_rivers.xml")
+        d = d["data"]
+        trees = d.pop("rivertrees")
         colormap = matplotlib.colormaps['terrain']
-        for x in d:
-            verts=d[x]["points"]
-            faces = [list(range(len(verts)))]
-            new_ob= panda_object_create_load.make_object(self.b, verts, faces, twosided=True)
-            el=d[x]["elevation"]
-            color=colormap(el)
-            new_ob.setColor(color)
-            new_ob.setPos(*vector.Vector(0,0,el))
-            #new_object.setScale(1,1,0.1)
-            #self.placed=True
-            
-            
         
+        my_river_tuple_list = self.recursive_river_tree_unpack(trees)
+        my_river_tuple_list = list(set(my_river_tuple_list))
+        
+        
+        
+        all_verts = []
+        vert_to_cell = {}
+        for x in d:
+            verts = d[x]["points"]
+            for vert in verts:
+                if vert not in vert_to_cell:
+                    vert_to_cell[vert]=[]
+                if x not in vert_to_cell[vert]:
+                    vert_to_cell[vert].append(x)
+            all_verts += verts
+        all_verts = list(set(all_verts))
+        
+        # figure out elevation and spawn basic surfaces
+        for x in d:
+            
+            # I should decide here if I'm creating a flat point
+            
+            my_center = vector.Vector(0,0,0)
+            
+            org_verts = d[x]["points"]
+            new_verts = []
+            for vert in org_verts:
+                my_center += vector.Vector(*vert)
+                z=0
+                for cell in vert_to_cell[vert]:
+                    z += d[cell]["elevation"]
+                z = z / len(vert_to_cell[vert])
+                new_vert = vector.Vector(*vert[:2],z)
+                new_verts.append(new_vert)
+            verts = new_verts
+            my_center = my_center / len(org_verts)
+            # no, I need these values anyway.
+            
+            
+            # ok this doesn't exactly work, I need
+            
+            flat = True
+            my_edge_list = []
+            if d[x]["river value"]>1:
+                flat = False
+                my_edge_list = []
+                for n in d[x]["neighbors"]:
+                    if (x,n) not in my_river_tuple_list:
+                        continue
+                    edge_tuple = d[x]["neighbors"][n]
+                    print(d[x]["neighbors"])
+                    print("confirm, this is edge tuple",edge_tuple)
+                    neighbor_value = d[n]["river value"]
+                    if neighbor_value >1:
+                        my_edge_list+=[list(edge_tuple[0])]
+            #flat=True
+            if flat:
+                faces = [list(range(len(verts)))]
+                new_ob = panda_object_create_load.make_object(self.b, verts, faces, twosided=True)
+                el=d[x]["elevation"]
+                color=colormap(el)
+                new_ob.setColor(color)
+                #new_ob.setPos(*vector.Vector(0,0,el))
+                #new_object.setScale(1,1,0.1)
+                #self.placed=True
+            else:
+                if True:
+                    faces = [list(range(len(verts)))]
+                    new_ob = panda_object_create_load.make_object(self.b, verts, faces, twosided=True)
+                    el=d[x]["elevation"]
+                    #color=colormap(el)
+                    new_ob.setColor((0.7,0.7,0.95,0.8))
+                    new_ob.setPos(0,0,-0.1)
+                # this is skipping the "last vertex" when creating 
+                # faces.
+                
+                r =  house.make(verts,my_edge_list,"world","river_short_absolute",cap_ends=False)
+                house_verts, faces = r 
+                new_house_verts = []
+                
+                #my_center = d[x]["center"]
+                if False:
+                    for vert in house_verts:
+                        
+                        # this is kind of useless, it fetches the first one correctly, but that's it.
+                        my_triangle = find_relevant_triangle(vert,verts)
+                        
+                        other = my_triangle[0][1]
+                        
+                        if False:
+                            newz = 0
+                            dist_sum = 0
+                            print(other)
+                            other  = tuple((other[0],other[1],0))
+                            for neighbor in vert_to_cell[other]:
+                                neighborverts = d[neighbor ]["points"]
+                                that_cell_center = vector.Vector(0,0,0)
+                                for vert in neighborverts:
+                                    that_cell_center += vector.Vector(*vert)
+                                that_cell_center = that_cell_center / len(neighborverts )
+                                dx = that_cell_center - other
+                                dist_x = dx.magnitude()
+                                dist_sum += dist_x
+                                newz +=  d[neighbor]["elevation"] * dist_x
+                            newz = newz/dist_sum
+                            
+                            z = newz
+                        
+                        if False:
+                            other = vector.Vector(*other)
+                            dvec1 = my_center - vector.Vector(*vert)
+                            dvec2 = other  - vector.Vector(*vert)
+                            
+                            dist1 = dvec1.magnitude()
+                            dist2 = dvec2.magnitude()
+                            
+                            full_dist = dist1 + dist2
+                            
+                            z = round(dist1/full_dist,0) * d[x]["elevation"] + round(dist2/full_dist,0) * other[2]
+                            
+                        old_z = float(vert[2])
+                        
+                        if True:
+                            z = bary_centric_interpolation(vert,my_triangle[0][1],my_triangle[1][1],my_triangle[2][1])
+                        new_house_vert = vector.Vector(vert[0],vert[1],z+old_z)#
+                        new_house_verts.append(new_house_vert)
+                        
+                        house_verts = new_house_verts
+                new_ob = panda_object_create_load.make_object(self.b, house_verts, faces, twosided=False)
+                el=d[x]["elevation"]
+                color=colormap(el)
+                new_ob.setColor(color)
+                #new_ob.setPos(*vector.Vector(0,0,el))
+                # figure out where my connections to other cells are
+                # mostly via
+                # "what's my score, what's their score."
+                # "if score" > 5
+                # mark the connection.
+                # this actually highlights that I probably need
+                # a different set of keypoints, depending on the 
+                # exit of the thing.
+                a=1
+            
+            
         
     def init_box_draw_vars(self):
         self.first_point3d=None
@@ -181,9 +436,10 @@ class MyEditor:
     def build_buttons(self):
         b1 = pig.create_custom_button("save markers",(-0.4,0,-0.9),self.save,[])
         b2 = pig.create_custom_button("load",(-0.0,0,-0.9),self.load,[])
-        b2 = pig.create_custom_button("load terrain",(0.2,0,-0.9),self.load,[])
+        b3 = pig.create_custom_button("load terrain",(0.2,0,-0.9),self.load,[])
         b1.setScale(0.05)
         b2.setScale(0.05)
+        b3.setScale(0.05)
     
     def load_terrain(self):
         with open("myterrain.csv","r") as f:
@@ -253,21 +509,7 @@ class MyEditor:
             
             tex = loader.loadTexture("colorgrid.png")
             verts, faces = r 
-            if False:
-                new_faces =[]
-                for face in faces:
-                    try:
-                        for vert in face:
-                            verts[vert]
-                    except:
-                        continue
-                    new_faces.append(face)
-                
-                faces=new_faces
-                print(faces)
-            #faces.remove([])
-            #for x in irtp:
-                #faces.remove(x)
+            
             #new_object = panda_object_create_load.make_object(self.b, points, twosided=True,texture=tex)
             new_object = panda_object_create_load.make_object(self.b, verts, faces, twosided=False)
             new_object.setPos(*vector.Vector(*pos)+vector.Vector(0,0,0.5))
